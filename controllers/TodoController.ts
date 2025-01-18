8470d4c10343280a155c311de97c09dc8d58b038
James-Hong-Jey <jameshong8387@gmail.com>
2025-01-17 18:10:32 -0800

    Fix IP Address

--- controllers/TodoController.ts
+++ controllers/TodoController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import * as os from "os";
 import { client } from "../services";
 import { ObjectId } from "mongodb";
 
@@ -32,7 +33,25 @@ export class TodoController {
     }
 
     async getIP(req: Request, res: Response, next: NextFunction) {
-        res.status(200).send(`Your IP is: ${req.ip}`);
+        const networkInterfaces = os.networkInterfaces();
+
+        // To extract the IP address (you may want to filter by 'eth0', 'wlan0', or another network interface)
+        for (const interfaceName in networkInterfaces) {
+            const interfaces = networkInterfaces[interfaceName];
+            if (!interfaces) {
+                continue;
+            }
+
+            for (const iface of interfaces) {
+                // Check if it's an IPv4 address (not IPv6)
+                if (iface.family === 'IPv4' && !iface.internal) {
+                    console.log(`IP Address of ${interfaceName}: ${iface.address}`);
+                }
+            }
+        }
+        const wifiInterfaces = networkInterfaces["Wi-Fi"];
+        const wifiAddress = wifiInterfaces && wifiInterfaces[1] ? wifiInterfaces[1].address : "Not Available";
+        res.status(200).send(`Your IP is: ${wifiAddress}`);
     }
 
     async getServerLocalTime(req: Request, res: Response, next: NextFunction) {
